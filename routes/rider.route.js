1ec1fed4c0a7f7382efdb3089850bb35864f5811
Arafat Rahman <arafat.rahman.6003@gmail.com>
2026-03-16 22:42:51 +0600

    commit

--- routes/rider.route.js
+++ routes/rider.route.js
@@ -9,13 +9,11 @@ import {
   logout,
   getProfile,
   getRiderStats,
-  sendOtp,
 } from "../controllers/rider.controller.js";
 import { authMiddlewareRider } from "../middlewares/auth.middleware.js";
 
 const riderRouter = express.Router();
 
-riderRouter.post("/send-otp", sendOtp);
 riderRouter.post("/register", registerRiderValidation, registerRider);
 riderRouter.post("/login", loginRiderValidation, loginRider);
 riderRouter.get("/logout", logout);
